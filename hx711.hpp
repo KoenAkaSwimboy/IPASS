65857dd4ae21899f4bed3897262080272597a025
koenakaswimboy <koen_schreur@hotmail.com>
2019-06-27 01:20:19 -0700

    Changed calibrate

--- hx711.hpp
+++ hx711.hpp
@@ -7,7 +7,7 @@ private:
 	unsigned long Count=0;
 	unsigned long grams=0;
 	unsigned long calGrams; 
-	long sample=0; 
+	unsigned long sample=0; 
 	long val=0;
 	unsigned long test=0;
 	hwlib::pin_in_out & DT;
@@ -22,72 +22,60 @@ public:
 		calSw( calSw ),
 		toGramsBool( toGramsBool ) {}
 
-unsigned long readCount(){
-	DT.direction_set_output();
-	DT.direction_flush();
-    DT.write(1);
-    SCK.write(0);
-    DT.direction_set_input();
-    DT.direction_flush();
-	DT.refresh();
-	unsigned char i;
-    while(DT.read()){
-        for(i=0; i<24; i++){	
-            SCK.write(1);
-            Count = Count << 1;
-            SCK.write(0);
-            DT.refresh();
-            if(DT.read()){
-				Count++;
-            }
-        }
-    }
-    SCK.write(1);
-    Count=Count^0x800000;
-    SCK.write(0);
-	return Count;
-}
-
-unsigned long toGrams(unsigned long grams, long sample, float val){
-		unsigned long som = (((grams-sample)/val)-2*((grams-sample)/val));
-		hwlib::cout<<" sample: " << sample << " grams: " << grams << "\n som" << som;
-		return(((grams-sample)/val)-2*((grams-sample)/val));	
-}
-
-void calibrate(unsigned long calGrams){
-	hwlib::cout <<"in calibrate\n";
-	for(int i=0; i<100; i++){
-		calGrams=readCount();	//pak de laatste waarde
-		sample+=calGrams; 			//ken de laatste waarde toe aan sample
-		hwlib::cout<<"sample: "<< sample << " grams: " << calGrams << " :: ";
-	}
-	sample/=100;		//gemiddelde van 100x
-	hwlib::cout<< "please put 48g on the weightscale and press the blue button\n";
-	while(true){
-		if(calSw.read()){
-			// while(test<1000){
-			// 	test=readCount();			//pak de laatste waarde
-			// 	test=sample-test;		//als het verschil tussen het gemiddelde en de laatste waarde groter is dan 1000 ga door
-				for(int j=0; j<100; j++){
-					calGrams=readCount();	//pak de laatste waarde	
-					val+=calGrams - sample;		//voor 100x voeg het verschil tussen de laatste waarde en het gemiddelde aan elkaar toe
+	unsigned long readCount(){
+		DT.direction_set_output();
+		DT.direction_flush();
+		DT.write(1);
+		SCK.write(0);
+		DT.direction_set_input();
+		DT.direction_flush();
+		DT.refresh();
+		unsigned char i;
+		while(DT.read()){
+			for(i=0; i<24; i++){	
+				SCK.write(1);
+				Count = Count << 1;
+				SCK.write(0);
+				DT.refresh();
+				if(DT.read()){
+					Count++;
 				}
-				val=val/100;						//bereken het gemiddelde
-				val=val/48; 						//calibratie gewicht
-				// int som = (((calGrams-sample)/val)-2*((calGrams-sample)/val));
-				int som = calGrams/val;
-					hwlib::cout<<"sample: " << sample << " grams: " << calGrams << "\n val: " << val << " som: " << som;
+			}
+		}
+		SCK.write(1);
+		Count=Count^0x800000;
+		SCK.write(0);
+		return Count;
+	}
+
+	void calibrate(unsigned long calGrams){
+		// hwlib::cout <<"in calibrate\n";
+		sample=0;
+		hwlib::cout<<sample;
+		for(unsigned int i=0; i<100; i++){
+			calGrams=readCount();	//pak de laatste waarde
+			sample=calGrams+sample; 			//ken de laatste waarde toe aan sample
+			hwlib::cout<<" grams: " << calGrams <<" sample: "<< sample << " :: ";
+		}
+		sample/=100;		//gemiddelde van 100x
+		hwlib::cout<< "please put 48g on the weightscale and press the blue button\n";
+		while(true){
+			if(calSw.read()){
+				// while(test<1000){ //check of er iets op de weegschaal wordt gezet
+				// 	test=readCount();			
+				// 	test=sample-test;		
+					for(int j=0; j<100; j++){
+						calGrams=readCount();	//pak de laatste waarde	
+						val= val + calGrams + sample;		//voor 100x voeg het verschil tussen de laatste waarde en het gemiddelde aan elkaar toe
+						//  hwlib::cout<<"sample: "<< sample << " grams: " << calGrams << " val: " << val <<" :: ";
+					}
+					val = val - sample;
+					val=val/100;						//bereken het gemiddelde
+					val=val/48; 						//calibratie gewicht
+					// int som = (((calGrams-sample)/val)-2*((calGrams-sample)/val));
+					int som = calGrams/val;
+						hwlib::cout<<"sample: " << sample << " grams: " << calGrams << "\n val: " << val << " som: " << som;
+			}
 		}
 	}
-}
 };
-
-// if(firstT){
-// 		hwlib::cout<< "please put 48g on the weightscale and press the blue button\n";
-// 		while(firstT){
-// 			if(calSw.read()){
-// 				calibrate(Count);
-// 				firstT=false;
-// 			}
-// 		}
-// 	}
