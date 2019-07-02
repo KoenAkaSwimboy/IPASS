ecdb6b80f67a633b250833dedd7651c30e0a196a
koenakaswimboy <koen_schreur@hotmail.com>
2019-07-02 00:34:34 -0700

    Added Doxygen

--- weightscale.hpp
+++ weightscale.hpp
@@ -3,19 +3,63 @@
 
 #include "hx711.hpp"
 
+///@file
+
+///\brief
+///Weightscale
+///\details
+///This a weightscale that gives back the number of grams that is
+///on the physical weightscale. This class is a subclass of 
+///the hx711 class.
+///The avg, oneGram, confirmSw, startSw and calWeight components
+///are private attributes. 
+///The appropiate constructers and functions are provided
+
 class weightscale : public hx711::hx711{
 private:
-	long avg; 
-	int oneGram;
+	long avg=0; 
+	int oneGram=0;
 	hwlib::pin_in & confirmSw;
 	hwlib::pin_in & startSw;
 	int calWeight=0;
 
 public:
+
+	///\brief
+	///default constructor
+	///\details
+	///This constructor gives the DT en SCK to the class HX711
+	///and initialize the confirmSw and startSw attributes as
+	///switches (pin_in) adn initialize the calWeight attribute as
+	///an integer. 
 	weightscale(hwlib::pin_in_out & DT, hwlib::pin_out & SCK, hwlib::pin_in & confirmSw, hwlib::pin_in & startSw, int calWeight);
 
+	///\brief
+	///Calibrate the weightscale
+	///\details
+	///This function calibrates the weightscale, first
+	///it takes the average of 100 times of the current data
+	///from the HX711 chip and allocate this to avg. Then it 
+	///asks the user to put the calibrate weight on the 
+	///weightscale. Again it takes the avarage of 100 times
+	///of the current data from the HX711 chip and allocate this
+	///to oneGram. Next it takes avg out of oneGram to get the
+	///difference between the average of '0' gram and the calibrate
+	///weight. It divide this number by the calibrate weight in grams
+	///and returns this number.
 	int calibrate();
+
+	///\brief
+	///Get the weight in grams
+	///\details
+	///This function takes the average of 25 times of
+	///the current data from the HX711 chip and devides
+	///this by oneGram to get the number of grams and returns
+	///this number.
 	long getWeight(int onegram);
+
+	///\brief
+	///start the application
 	void start();
 };
 
