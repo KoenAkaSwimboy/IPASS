0808eea7644fe2c8033a6ad61102c6b338a1622e
Koesch <koen_schreur@hotmail.com>
2019-07-17 11:57:57 +0200

    Added Doxygen

--- hx711.hpp
+++ hx711.hpp
@@ -9,7 +9,7 @@
 ///HX711
 ///\details
 ///HX711 returns the data from the chip.
-///The Count, DT and SCK components are protected attributes 
+///The Data, DT and SCK components are protected attributes 
 ///HX711 is the supperclass of weightscale).
 ///The appropiate constructers and functions are provided
 class hx711{
@@ -18,7 +18,7 @@ private:
 	uint8_t GAIN;
 	int tries;
 	int maxT=500;				//default is 500
-	unsigned long Count;
+	unsigned long Data;
 	hwlib::pin_in & DT;
 	hwlib::pin_out & SCK;
 	unsigned int times=100;		//default is 100
@@ -38,49 +38,87 @@ public:
 	///attribute as an output (pin_out).
 	hx711(hwlib::pin_in & DT, hwlib::pin_out & SCK);
 
+	///\brief
+	///Check if the chip is ready for retrieval
+	///\details
+	///The chip is ready for retrieval when the data pin is low,
+	///see datasheet.
 	bool isReady();
 
+	///\brief
+	///Wait till the chip is ready for retrieval
 	bool waitReady();
 
+	///\brief
+	///Set the gain, see datasheet
 	void setGain(int gain);
 	
+	///\brief
+	///Power the chip
+	///\details
+	///To turn the chip on, SCK pin must be low, see datasheet
 	void powerOn();
 
+	///\brief
+	///Power the chip down, see datasheet
 	void powerDown();
 
+	///\brief
+	///Start the chip
+	///\details
+	///First set the gain, then turn the chip on.
 	virtual void start(int gain);
 	
 	///\brief
 	///Get the data from the chip
 	///\details
-	///This function gets the data from the chip. The function
-	///sets DT (Data pin) as an output and sets it to 1 and the SCK
-	///to 0. After this it sets DT as an input, it sets Count to 0
-	///to make sure there isn't a random value in Count. While DT
-	///is 1, pulse the clock 24 times to read the data. Return 
-	///the data. this code is from the datasheet of the HX711 chip.
+
 	unsigned long read();
 
+	///\brief
+	///Make the chip ready for the next conversion
+	///\details
+	///Depending on the gain, 
 	void nextConver();
 
+	///\brief
+	///Calculate an average over several measurements
 	unsigned long readAvg();
 
+	///\brief
+	///Take the average over several measurements and set this as the tare
 	void tare();
 
+	///\brief
+	///Read the avrage of 100 times minus the offset
 	unsigned long getData();
 
+	///\brief
+	///getData() devide through the calibration weight
 	unsigned long getWeight();
 
+	///\brief
+	///Get the 'empty' weight
 	unsigned long getTare();
 
+	///\brief
+	///Set the 'empty' weight
 	void setTare(unsigned long TARE);
 
+	///\brief
+	///Get the calibration weight
 	float getScale();
 
+	///\brief
+	///Set the calibration weight
 	void setScale(float SCALE);
 
+	///\brief
+	///Set the amount of measurements
 	void setTimes(int TIMES);
 
+	///\brief
+	///Set the maxium amount of tries
 	void setMaxT(int MAXT);
 };
 
